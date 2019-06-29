c7576d465a9028f5029fd24e8162901d26ae024b
dabao <1046859375@qq.com>
2019-06-29 21:17:02 +0800

    程达大傻逼

---
+++ src/main/java/com/itheima/test/demo.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+public class demo {
+
+    public static void main(String[] args) {
+
+        System.out.println("程达是个人");
+        System.out.println("程达是个傻逼");
+        System.out.println("程达是个大傻逼");
+
+    }
+}
